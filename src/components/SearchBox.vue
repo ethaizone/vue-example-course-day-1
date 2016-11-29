6f68fe844c478909ce6773df7f7b3a87aacab72c
EThaiZone <ethaizone@hotmail.com>
2016-11-29 18:33:38 +0700

    'change'

---
+++ src/components/SearchBox.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <input placeholder="Type keyword in me!" v-model="keyword">
+    <button v-show="keyword" @click="search">Search</button>
+  </div>
+</template>
+
+<script>
+import bus from './../bus'
+
+var contentData = {
+  content: 'homepage'
+}
+bus.$on('goto', function (item) {
+  contentData.content = item
+})
+
+export default {
+  name: 'search-box',
+  data () {
+    return {
+      keyword: null
+    }
+  },
+  methods: {
+    search () {
+      window.alert('You search ' + this.keyword)
+      this.keyword = null
+    }
+  }
+}
+</script>
+
+<style scoped>
+div {
+  margin-top: 20px;
+  border: solid 3px green;
+}
+</style>
